d8cab77996c251f565a021745c8e4115edd4290f
pauld <paul.dragoslav@gmail.com>
2019-11-06 19:40:51 +0000

    Resolve deprecation warning

--- src/main/java/com/example/paul/RestApplication.java
+++ src/main/java/com/example/paul/RestApplication.java
@@ -11,20 +11,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class RestApplication implements WebMvcConfigurer {
 
-   public static void main(String[] args) {
-      SpringApplication.run(RestApplication.class, args);
-   }
+    public static void main(String[] args) {
+        SpringApplication.run(RestApplication.class, args);
+    }
 
-   public void configureContentNegotiation(
-         ContentNegotiationConfigurer contentNegotiationConfigurer) {
-      contentNegotiationConfigurer
-            .favorPathExtension(false)
-            .favorParameter(false)
-            .parameterName("mediaType")
-            .ignoreAcceptHeader(false)
-            .useJaf(false)
-            .defaultContentType(MediaType.APPLICATION_JSON)
-            .mediaType("application/xml", MediaType.APPLICATION_XML)
-            .mediaType("application/json", MediaType.APPLICATION_JSON);
-   }
+    public void configureContentNegotiation(
+            ContentNegotiationConfigurer contentNegotiationConfigurer) {
+        contentNegotiationConfigurer
+                .favorPathExtension(false)
+                .favorParameter(false)
+                .parameterName("mediaType")
+                .ignoreAcceptHeader(false)
+                .useRegisteredExtensionsOnly(true)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("application/xml", MediaType.APPLICATION_XML)
+                .mediaType("application/json", MediaType.APPLICATION_JSON);
+    }
 }
